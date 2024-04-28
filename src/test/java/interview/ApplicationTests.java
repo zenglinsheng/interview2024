4e0cd3d0b0962c9a3a020430a8676dafb721e024
zenglsh <996433583@qq.com>
2024-04-28 14:33:09 +0800

    add: zk分布式锁

--- src/test/java/interview/ApplicationTests.java
+++ src/test/java/interview/ApplicationTests.java
@@ -7,17 +7,29 @@ import interview.iocFacade01.SupportType;
 import interview.iocFacade01.pay.PayFacade;
 import interview.iocFacade01.search.SearchFacade;
 import interview.redisDistributeLock.RedisLockWithSetNx;
+import interview.zkDistributeLock.ZkLock;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @SuppressWarnings("all")
 public class ApplicationTests {
 
+	private static final Logger log = LoggerFactory.getLogger(ApplicationTests.class);
 	@Autowired
 	private Teacher teacher;
 
@@ -62,5 +74,36 @@ public class ApplicationTests {
 		}
 	}
 
+	@Test
+	public void testZkDistributeLock() throws IOException {
+		ZkLock zkLock = new ZkLock();
+		boolean lock = zkLock.getLock("order");
+		if (lock) {
+			System.out.println("获取锁成功");
+		} else {
+			System.out.println("获取锁失败");
+		}
+	}
+
+	@Test
+	public void testCuratorLock() throws Exception {
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+		CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+		client.start();
+		InterProcessMutex lock = new InterProcessMutex(client, "/order");
+		try {
+			if (lock.acquire(10, TimeUnit.SECONDS)) {
+				try {
+					log.error("我获取了锁！");
+				} finally {
+					lock.release();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		client.close();
+
+	}
 
 }
